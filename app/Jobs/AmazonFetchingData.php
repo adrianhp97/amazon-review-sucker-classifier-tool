b173e8b88b78eb02808b98c99cbdde77c6cf135e
Adrian Hartarto p <adrianhp100797@gmail.com>
2018-08-23 09:09:39 +0700

    fixed bugged for queue jobs

--- app/Jobs/AmazonFetchingData.php
+++ app/Jobs/AmazonFetchingData.php
@@ -9,6 +9,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 
 use Scrapper;
+use Storage;
 use AmazonReviewSuckerClassifierTool\Review;
 
 class AmazonFetchingData implements ShouldQueue
@@ -36,7 +37,7 @@ class AmazonFetchingData implements ShouldQueue
     {
         $results = $this->getReviewByAsin($this->asin);
         foreach ($results as $review) {
-            if ($review != null) {
+            if (!array_key_exists('error', $review)) {
                 $review['asin'] = $this->asin;
                 $newReview = Review::create($review);
             }
@@ -46,52 +47,53 @@ class AmazonFetchingData implements ShouldQueue
     public function getReviewByAsin($asin)
 	{
         $counter = 1;
-        $isEndOfPage = False;
         $reviews = array();
-        while (!$isEndOfPage) {
+        $reviews_page = array(array('start' => True));
+        while (!array_key_exists('error', $reviews_page[0])) {
             $params = "/?ie=UTF8&sortBy=recent&pageNumber={$counter}";
             $crawler = Scrapper::request('GET', 'https://www.amazon.com/product-reviews/' . $asin . $params);
-            if ($crawler->filter('div#cm_cr-review_list > div')->count() > 0) {
-                $reviews_page = $crawler->filter('div#cm_cr-review_list > div')->each(function($review) {
-                    if ($review->filter('div.review div.celwidget')->count() > 0) {
-                        $title = $review->filter('div.review div.celwidget div:nth-child(1) a.review-title')->text();
-                        $title_link = $review->filter('div.review div.celwidget div:nth-child(1) a.review-title')->attr('href');
-                        $score = $review->filter('div.review div.celwidget div:nth-child(1) a')->attr('title');
-                        $date = $review->filter('div.review div.celwidget div:nth-child(2) span.review-date')->text();
-                        $author = $review->filter('div.review div.celwidget div:nth-child(2) span.review-byline a.author')->text();
-                        $author_link = $review->filter('div.review div.celwidget div:nth-child(2) span.review-byline a.author')->attr('href');
-                        if ($review->filter('div.review div.celwidget div.review-comments span.review-comment-total')->count() > 0) {
-                            $number_of_comments = $review->filter('div.review div.celwidget div.review-comments span.review-comment-total')->text();
-                        } else {
-                            $number_of_comments = 0;
-                        }
-                        $photo = $review->filter('div.review div.celwidget div.review-data div.review-image-container')->count() > 0;
-                        $video = $review->filter('div.review div.celwidget div.review-data div.review-video-container')->count() > 0;
-                        $photos_or_video = $photo && $video;
-                        $verified = $review->filter('div.review div.celwidget div:nth-child(3) span.a-declarative')->count() > 0;
-                        $child_product = False;
-                        $child_asin = False;
-                        return [
-                            'title' => $title,
-                            'title_link' => $title_link,
-                            'score' => (int)$score[0],
-                            'date' => $this->stringToDate($date),
-                            'author' => $author,
-                            'author_link' => $author_link,
-                            'number_of_comments' => $number_of_comments,
-                            'photos_or_video' => $photos_or_video,
-                            'verified' => $verified,
-                            'child_product' => $child_product,
-                            'child_asin' => $child_asin
-                        ];
+            $reviews_page = $crawler->filter('div#cm_cr-review_list > div')->each(function($review, $isEndOfPage) {
+                if ($review->filter('.no-reviews-section')->count() > 0) {
+                    return [
+                        'error' => True
+                    ];
+                } else if ($review->filter('div.review div.celwidget')->count() > 0) {
+                    $title = $review->filter('div.review div.celwidget div:nth-child(1) a.review-title')->text();
+                    $title_link = $review->filter('div.review div.celwidget div:nth-child(1) a.review-title')->attr('href');
+                    $score = $review->filter('div.review div.celwidget div:nth-child(1) a')->attr('title');
+                    $date = $review->filter('div.review div.celwidget div:nth-child(2) span.review-date')->text();
+                    $author = $review->filter('div.review div.celwidget div:nth-child(2) span.review-byline a.author')->text();
+                    $author_link = $review->filter('div.review div.celwidget div:nth-child(2) span.review-byline a.author')->attr('href');
+                    if ($review->filter('div.review div.celwidget div.review-comments span.review-comment-total')->count() > 0) {
+                        $number_of_comments = $review->filter('div.review div.celwidget div.review-comments span.review-comment-total')->text();
+                    } else {
+                        $number_of_comments = 0;
                     }
-                });
-                $counter++;
-                $reviews = array_merge($reviews, $reviews_page);
-                sleep(rand(3,10));
-            } else {
-                $isEndOfPage = True;
-            }
+                    $photo = $review->filter('div.review div.celwidget div.review-data div.review-image-container')->count() > 0;
+                    $video = $review->filter('div.review div.celwidget div.review-data div.review-video-container')->count() > 0;
+                    $photos_or_video = $photo && $video;
+                    $verified = $review->filter('div.review div.celwidget div:nth-child(3) span.a-declarative')->count() > 0;
+                    $child_product = False;
+                    $child_asin = False;
+
+                    return [
+                        'title' => $title,
+                        'title_link' => $title_link,
+                        'score' => (int)$score[0],
+                        'date' => $this->stringToDate($date),
+                        'author' => $author,
+                        'author_link' => $author_link,
+                        'number_of_comments' => $number_of_comments,
+                        'photos_or_video' => $photos_or_video,
+                        'verified' => $verified,
+                        'child_product' => $child_product,
+                        'child_asin' => $child_asin
+                    ];
+                }
+            });
+            $counter++;
+            $reviews = array_merge($reviews, $reviews_page);
+            sleep(rand(3,10));
         }
         return $reviews;
     }
