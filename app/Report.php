635b01386c6b68a28ab12d495a265241dbb374ae
Adrian Hartarto p <adrianhp100797@gmail.com>
2018-09-05 21:39:14 +0700

    add reports table

---
+++ app/Report.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace AmazonReviewSuckerClassifierTool;
+
+use AmazonReviewSuckerClassifierTool\Traits\Uuids;
+use Illuminate\Database\Eloquent\Model;
+
+class Report extends Model
+{
+    use Uuids;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'reports';
+
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    protected $fillable = [
+        'asin',
+        'done',
+    ];
+}
